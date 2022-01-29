f033432bee87e1a6fad4e3f0fbaad8b11fa04b15
TheClashFruit <clash.fruit123@gmail.com>
2022-01-29 16:42:23 +0100

    feat: add types

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+export declare class Sketchub {
+  getCategories(callback: Function): null
+  getProjectDetails(id: Number, callback: Function): null
+  getProjectList(page: Number, callback: Function): null
+}
