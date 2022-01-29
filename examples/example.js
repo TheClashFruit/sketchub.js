e523cb8ab66f64c04e14a416cd565dd3e1366f31
TheClashFruit <clash.fruit123@gmail.com>
2022-01-29 16:31:36 +0100

    feat: move example.js

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Sketchub } = require('./index');
+const { Sketchub } = require('../index');
 const apiClient = new Sketchub();
 
 apiClient.setApiKey(process.env.API_KEY);
